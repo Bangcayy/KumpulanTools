2327239cabb3496a7e8c04681be345dab4e029e3
Cy☆57 <158684847+Bangcayy@users.noreply.github.com>
2024-02-03 12:03:00 +0700

    Add files via upload

---
+++ bangcayy.py
@@ -0,0 +1,25 @@
+input ("ketik ENTER bang :")                       
+print ("\n▒▓█ WELCOME TO CY☆57 █▓▒")
+print ("\nHALO BANG")   
+print ("Author : Cy☆57")
+print ("============================================")
+print ("\n Selamat Datang Di Kumpulan Tools Orang :")
+
+print ("\n1.DDos Ripper")
+print ("2.RED_HAWK")
+print ("3.Sqlmap")
+print ("4.Ddos Wifi")
+print ("5.VulnX")
+
+nomor = int(input("\nPilih sesuai angka :"))
+
+if nomor == 1:
+    print ("git clone https://github.com/palahsu/DDoS-Ripper.git")
+elif nomor == 2:
+    print ("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+elif nomor == 3:
+        print ("git clone https://github.com/sqlmapproject/sqlmap.git")
+elif nomor == 4:
+            print ("git clone https://github.com/Alygnt/fl00d-wifi") 
+elif nomor == 5:
+    print ("git clone https://github.com/anouarbensaad/vulnx")
